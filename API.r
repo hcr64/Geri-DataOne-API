ecec4d0059488b788493f6967412f8f77ae1a78a
hcr64 <149124502+hcr64@users.noreply.github.com>
2025-09-17 09:17:42 -0700

    Create API.r and added some notes

---
+++ API.r
@@ -0,0 +1,23 @@
+# install the DataOne Packacge
+install.packages("dataone")
+
+# include the library
+library(dataone)
+
+# connect to a coordinating node
+
+# then connect that corrdinating node to a member node\
+
+# get a file path to what will be uploaded
+
+# get a unique identifier for uploading the data
+
+# get the file/data formate (eg. csv)
+
+# put the metadata into an object
+
+# put orcid int0 the object too
+
+# read the file 
+
+# upload it
