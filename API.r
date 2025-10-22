335c8e7593e0c51db565463572fbcc640e646e54
hcr64 <149124502+hcr64@users.noreply.github.com>
2025-10-22 12:20:10 -0700

    Update API.r

--- API.r
+++ API.r
@@ -5,16 +5,17 @@
 # include the libraries
 library(dataone)
 library(datapack)
+library(uuid)
 
 # connect to a coordinating node, set to PROD for staging data
-coord_node <- CNode( "PROD" )
+coord_node <- CNode( "STAGING" )
 
 # then connect that coordinating node to a member node to upload data to
-member_node <- getMNode( coord_node, "" )
+member_node <- getMNode( coord_node, "urn:node:mnSandboxUCSB1" )
 
 # get a file path to what will be uploaded, get a metadat path too
-local_data_path = ""
-metadata_path = ""
+local_data_path = "data.csv"
+metadata_path = "metadata.csv"
 
 # get the file/data format (eg. csv)
 data_format <- "file/csv"
